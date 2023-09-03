97f4fb52eadb6d16fbdc06cc9274d5646a2db577
MartinWWDC <60852345+MartinWWDC@users.noreply.github.com>
2023-09-03 13:10:23 +0200

    Update mainEsame.ino

--- mainEsame/mainEsame.ino
+++ mainEsame/mainEsame.ino
@@ -2,104 +2,60 @@
 
 LiquidCrystal_I2C lcd(0x27,16,2);
 
+int sensor_pins[]={9,10,11,12,13};
 
-int g2_pin_const=9;
-int g3_pin_const=10;
-int g4_pin_const=11;
-int g5_pin_const=12;
-int g6_pin_const=13;
+int g_old[]={1,1,1,1,1};
 
 
+int g_c[]={0,0,0,0,0};
 
-int g1_c=0;
-int g2_c=0;
-int g3_c=0;
-int g4_c=0;
-int g5_c=0;
-int g1_old=1;
-int g2_old=1;
-int g3_old=1;
-int g4_old=1;
-int g5_old=1;
-
+int monitor_cord[sizeof(sensor_pins)][2]={{0,0},{5,0},{11,0},{0,1},{5,1}};
 
 void setup() {
     setUpMonitor();    
 
     setUpSensor();
-
-
+    //Serial.begin(9600);
+    
 }
  
 void loop(){
-  bool c=false;
-  int g1=digitalRead(g2_pin_const);
-  int g2=digitalRead(g3_pin_const);
-  int g3=digitalRead(g4_pin_const);
-  int g4=digitalRead(g5_pin_const);
-  int g5=digitalRead(g6_pin_const);
-
   
-  if(g1<g1_old){
-    c=true;
-    g1_c++;
-  } 
-  if(g2<g2_old){
-     c=true;
-     g2_c++;
-
-
-  } 
-  if(g3<g3_old){
-     c=true;
-     g3_c++;
-
-
-  } 
-  if(g4<g4_old){
-    c=true;
-    g4_c++;
-
-
-  } 
-  if(g5<g5_old){
-    c=true;
-    g5_c++;
+  bool c=false;
+  for (int i = 0; i < sizeof(sensor_pins) / sizeof(sensor_pins[0]); i++) {
+      
+    int g_n=digitalRead(sensor_pins[i]);
+  
+    if(g_n<g_old[i]){
+      c=true;
+      g_c[i]++;
+    } 
+   
+    g_old[i]=g_n;
+  
+    if(c){
+     updateMonitor();    
+    }
   }
-  g1_old=g1;
-  g2_old=g2;
-  g3_old=g3;
-  g4_old=g4;
-  g5_old=g5;
-  if(c){
-   updateMonitor();    
-  }
+
 }
 
 
 void setUpSensor(){
-    pinMode (g2_pin_const, INPUT_PULLUP);
-    pinMode (g3_pin_const, INPUT_PULLUP);
-    pinMode (g4_pin_const, INPUT_PULLUP);
-    pinMode (g5_pin_const, INPUT_PULLUP);
-    pinMode (g6_pin_const, INPUT_PULLUP);
+
+  for (int i = 0; i < sizeof(sensor_pins) / sizeof(sensor_pins[0]); i++) {
+      pinMode(sensor_pins[i], INPUT_PULLUP);
+  }
+
+
 }
 void updateMonitor(){
-    lcd.setCursor(0,0); // Move the cursor to row 1, column 0
-    lcd.print("f1:"); // The count is displayed every second
-    lcd.print(g1_c);
-    lcd.setCursor(5,0); // Move the cursor to row 1, column 0
-    lcd.print("g2:"); // The count is displayed every second
-    lcd.print(g2_c);
-    lcd.setCursor(11,0); // Move the cursor to row 1, column 0
-    lcd.print("g3:"); // The count is displayed every second
-    lcd.print(g3_c);
-    lcd.setCursor(0,1); // Move the cursor to row 1, column 0
-    lcd.print("g4:"); // The count is displayed every second
-    lcd.print(g4_c); 
-    lcd.setCursor(5,1); // Move the cursor to row 1, column 0
-    lcd.print("g5:"); // The count is displayed every second
-    lcd.print(g5_c);
+    //Serial.println(sizeof(sensor_pins));
+    for (int i = 0; i < sizeof(sensor_pins) / sizeof(sensor_pins[0]); i++) {
+        lcd.setCursor(monitor_cord[i][0], monitor_cord[i][1]);
+        lcd.print("g" + String(i) + ":");
+        lcd.print(g_c[i]);
+    }
 
 }
 
